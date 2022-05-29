8bdda4203a38719fcdf7a1821373636db48ed807
Itajubá Segundo <itajuba.segundo@nubank.com.br>
2022-05-29 14:45:23 -0300

    Add counter component

--- src/reframe_basic_crud/home/views.cljs
+++ src/reframe_basic_crud/home/views.cljs
@@ -1,6 +1,7 @@
 (ns reframe-basic-crud.home.views
   (:require
    [re-com.core :as re-com :refer [at]]
+   [reagent.core :as reagent]
    [reframe-basic-crud.routes :as routes]))
 
 ;; home
@@ -10,10 +11,29 @@
    :label (str "This is the Home Page.")
    :level :level1])
 
+(defn counter [counter-name initial-value] ; TODO a schema validation?
+  (let [counter-state (reagent/atom initial-value)]
+    (fn [] ; Notice this a form-2 component, but not sure if it is correct
+      [re-com/v-box
+       :children [[re-com/h-box
+                   :children [[:h3 counter-name ": " @counter-state]]]
+                  [re-com/h-box
+                   :children [[re-com/button
+                               :label "-"
+                               :on-click #(swap! counter-state dec)] ; one way to write anonymous function
+                              [re-com/button
+                               :label "+"
+                               :on-click (fn [] (swap! counter-state inc))]]]]])))
+
 (defn home-panel []
   [re-com/v-box
    :src      (at)
    :gap      "1em"
-   :children [[home-title]]])
+   :children [[home-title]
+              [counter "Counter 1" 0]
+              [re-com/line
+               :size "3px"
+               :color "blue"]
+              [counter "Counter 2" 10]]])
 
 (defmethod routes/panels :home-panel [] [home-panel])
